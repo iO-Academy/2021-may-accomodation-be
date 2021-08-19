70a953e2abaa080b21c1a9cc3eb630b004de1073
Saskia <saskia_west1996@hotmail.co.uk>
2021-08-19 13:41:21 +0100

    post request to insert user data with booked dates with dateRange function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const MongoClient = require('mongodb').MongoClient
 const ObjectId = require('mongodb').ObjectId
 const cors = require('cors')
+const dateRange = require('./dateRange')
 
 const url = "mongodb://root:password@localhost:27017"
 
@@ -37,16 +38,22 @@ app.get('/hotels', async (request, response) => {
     response.json(data)
 })
 
-
-// app.post('/booking-confirmation', async (request, response) => {
-//     const connection = await MongoClient.connect(url)
-//     const db = connection.db('seal')
-//     const collection = db.collection('hotels')
-//
-//     const newBooking = await collection.insertOne({booking: request.body.booking})
-//     response.json({success: newBooking.insertedId !== undefined})
-// })
-
+app.post('/hotels/:id', async (request, response) => {
+    const connection = await MongoClient.connect(url)
+    const db = connection.db('seal')
+    const collection = db.collection('hotels')
+    let userName = request.body.name
+    let userEmail = request.body.email
+    let userAdult_guests = request.body.adult_guests
+    let userChild_guests = request.body.child_guests
+    let userCheckinDate = request.body.checkin
+    let userCheckoutDate = request.body.checkout
+    let bookedDates = dateRange(userCheckinDate, userCheckoutDate)
+    const newBooking = await collection.updateOne({_id: ObjectId(request.params.id)},{$push: {bookings: {name: userName, email: userEmail, checkin: userCheckinDate, checkout: userCheckoutDate, adult_guests: userAdult_guests, child_guests: userChild_guests}}})
+    const newBookedDates = await collection.updateOne({_id: ObjectId(request.params.id)},{$push: {booked: {$each: bookedDates}}})
+    response.json({success: newBooking.modifiedCount === 1})
+    response.json({success: newBookedDates.modifiedCount === 1})
+})
 
 app.listen(port, () => {
     console.log('Server running')
